787b140e101acab04e8c7f699e14a2afcdbb05ad
Bill Rex <rexw3035@gmail.com>
2014-04-11 08:16:24 -0500

    Can now add a role to a user.

---
+++ lib/AddRole.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace techsterx\SlimOpensrs\API;
+
+class AddRole extends \techsterx\SlimOpensrs\API\BaseClass
+{
+	public static function validate($data)
+	{
+		$roles = array('company', 'domain', 'mail', 'workgroup');
+
+		if (empty($data['role']) || !in_array($data['role'], $roles)) {
+			trigger_error("oSRS Error - No role found\n", E_USER_WARNING);
+			return false;
+		}
+		
+		if (empty($data['user']) || empty($data['object'])) {
+			trigger_error("oSRS Error - User/Role/Object required\n", E_USER_WARNING);
+			return false;
+		}
+
+		return true;
+	}
+}
