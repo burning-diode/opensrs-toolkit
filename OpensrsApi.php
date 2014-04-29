4fc6c6053d58758a8b2cff70b31eaf169362670a
Bill Rex <rexw3035@gmail.com>
2014-04-29 12:06:07 -0500

    removed dependency on Slim

--- OpensrsApi.php
+++ OpensrsApi.php
@@ -4,36 +4,59 @@ namespace techsterx\SlimOpensrs;
 
 class OpensrsApi
 {
-	protected $app;
+	protected $credentials;
 
 	public function __construct()
 	{
-		$this->app = \Slim\Slim::getInstance();
+		$this->credentials = array(
+			'user' => null,
+			'password' => null,
+		);
+	}
+
+	public function __set($name, $value)
+	{
+		switch ($name) {
+			case 'user':
+			case 'password':
+				if (is_string($name)) {
+					$this->credentials[$name] = $value;
+				} else {
+					$this->trigger_error('Invalid type for property ' . $name);
+				}
+				break;
+			default:
+				$this->trigger_error('Undefined property ' . $name);
+				break;
+		}
+	}
+
+	public function __get($name)
+	{
+		if (array_key_exists($name, $this->credentials)) {
+			return $this->credentials[$name];
+		}
+
+		$this->trigger_error('Undefined property ' . $name);
+
+		return null;
 	}
 
 	public function __call($name, $data)
 	{
 		$arguments = array_merge(array(
-			'credentials' => $this->mailAuth(),
+			'credentials' => $this->credentials,
 		), $data[0]);
 
-		$response = call_user_func_array(array(__NAMESPACE__.'\\API\\'.$name, 'call'), array($arguments));
+		$response = call_user_func_array(array(__NAMESPACE__ . '\\API\\' . $name, 'call'), array($arguments));
 
 		return is_array($response) ? $response : json_decode($response);
 	}
 
-	private function mailAuth()
+	final private function trigger_error($msg, $level = E_USER_NOTICE)
 	{
-		$allowed_keys = array('user', 'password');
+		$trace = debug_backtrace();
 
-		$config = $this->app->config('opensrs.authentication');
-
-		foreach ($config['mail'] as $key => $value) {
-			if (!in_array($key, $allowed_keys)) {
-				unset($config['mail'][$key]);
-			}
-		}
-
-		return $config['mail'];
+		trigger_error($msg . ' in ' . $trace[0]['file'] . ' on line ' . $trace[0]['line'], $level);
 	}
 }
