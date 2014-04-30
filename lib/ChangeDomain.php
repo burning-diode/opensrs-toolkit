3bcf7129a11c010e745779d41d0d47250ac502e4
Bill Rex <rexw3035@gmail.com>
2014-04-30 12:06:21 -0500

    Added ChangeDomain

---
+++ lib/ChangeDomain.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace techsterx\SlimOpensrs\API;
+
+class ChangeDomain extends \techsterx\SlimOpensrs\API\BaseClass
+{
+	public static function validate($data)
+	{
+		if (empty($data['domain'])) {
+			trigger_error("oSRS Error - Domain required\n", E_USER_WARNING);
+		} else {
+			return true;
+		}
+	}
+}
